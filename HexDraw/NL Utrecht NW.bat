81f5b82ce95a0405a1736eede4ba2b95cac382a5
Harry <harry.vdk.82.04@gmail.com>
2014-08-23 14:46:31 +0200

    First version.

---
+++ HexDraw/NL Utrecht NW.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\ImageMagick-6.8.9-Q16\convert" "nl utrecht nw.png" -crop   8510x6024+152+71  "tmp.png"
+"C:\Program Files\ImageMagick-6.8.9-Q16\convert" "tmp.png"           -resize 3842x2720         "C:\Matrix Games\Flashpoint Campaigns Red Storm\Modules\FPRS\Maps\NL Utrecht NW.PNG"
