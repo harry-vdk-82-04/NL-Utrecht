f5c3c68f788622598456c0e331fde23c8cbe8b27
harry <harry.vdk.82.04@gmail.com>
2014-09-23 21:55:14 +0200

    Finish SW map

--- HexDraw/NL Utrecht NW.bat
+++ HexDraw/NL Utrecht NW.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\ImageMagick-6.8.9-Q16\convert" "nl utrecht nw.png"     -crop   8510x6024+152+71  "NL Utrecht NW.tmp.png"
-"C:\Program Files\ImageMagick-6.8.9-Q16\convert" "NL Utrecht NW.tmp.png" -resize 3842x2720         "C:\Matrix Games\Flashpoint Campaigns Red Storm\Modules\FPRS\Maps\NL Utrecht NW.png"
+"C:\Program Files\ImageMagick-6.8.9-Q16\convert" "nl utrecht nw.png"        -crop   8510x6024+152+71  "..\tmp\nl utrecht nw.png"
+"C:\Program Files\ImageMagick-6.8.9-Q16\convert" "..\tmp\nl utrecht nw.png" -resize 3842x2720         "C:\Matrix Games\Flashpoint Campaigns Red Storm\Modules\FPRS\Maps\NL Utrecht NW.png"
--- HexDraw/NL Utrecht SW.bat
+++ HexDraw/NL Utrecht SW.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\ImageMagick-6.8.9-Q16\convert" "nl utrecht sw.png"     -crop   8510x6024+152+71  "nl utrecht sw.tmp.png"
-"C:\Program Files\ImageMagick-6.8.9-Q16\convert" "nl utrecht sw.tmp.png" -resize 3842x2720         "C:\Matrix Games\Flashpoint Campaigns Red Storm\Modules\FPRS\Maps\NL Utrecht SW.png"
+"C:\Program Files\ImageMagick-6.8.9-Q16\convert" "nl utrecht sw.png"        -crop   8510x6024+152+71  "..\tmp\nl utrecht sw.png"
+"C:\Program Files\ImageMagick-6.8.9-Q16\convert" "..\tmp\nl utrecht sw.png" -resize 3842x2720         "C:\Matrix Games\Flashpoint Campaigns Red Storm\Modules\FPRS\Maps\NL Utrecht SW.png"
